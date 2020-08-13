215fb5e14b169b1a9790f658d52bc1858cc3e2d5
efe <efe@efes-pro.speedport_w_724v_09011603_06_006>
2020-08-13 15:17:49 +0200

    solve

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='mlproject',
       packages=find_packages(),
       test_suite = 'tests',
       # include_package_data: to install data from MANIFEST.in
+      install_requires=requirements,
       include_package_data=True,
-      scripts=['scripts/mlproject-run'],
+      scripts=['scripts/mlproject-run', 'scripts/mlproject-computedist'],
       zip_safe=False)
---
+++ tests/tools_test.py
@@ -0,0 +1,4 @@
+from mlproject.tools import haversine
+
+def test_haversine_works():
+	assert haversine(1,2,3,4)
